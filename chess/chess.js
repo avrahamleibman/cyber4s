9f25334bf459c76c4833dad0812ce6bf6a054ae5
avrahamleibman <102976517+avrahamleibman@users.noreply.github.com>
2022-04-20 19:46:55 +0300

    rewrote js + added possible moves

--- chess/chess.js
+++ chess/chess.js
@@ -1,101 +1,131 @@
-let vClicked;
 
-let aPieces = []
-let srcPieces = {
-    bRook: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Chess_rdt45.svg/50px-Chess_rdt45.svg.png",
-    bKnight: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Chess_ndt45.svg/50px-Chess_ndt45.svg.png",
-    bBishop: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Chess_bdt45.svg/50px-Chess_bdt45.svg.png",
-    bQueen: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Chess_qdt45.svg/50px-Chess_qdt45.svg.png",
-    bKing: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Chess_kdt45.svg/50px-Chess_kdt45.svg.png",
-    bPawn: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Chess_pdt45.svg/50px-Chess_pdt45.svg.png",
-    wPawn: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Chess_plt45.svg/50px-Chess_plt45.svg.png",
-    wRook: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Chess_rlt45.svg/50px-Chess_rlt45.svg.png",
-    wKnight: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Chess_nlt45.svg/50px-Chess_nlt45.svg.png",
-    wBishop: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Chess_blt45.svg/50px-Chess_blt45.svg.png",
-    wQueen: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Chess_qlt45.svg/50px-Chess_qlt45.svg.png",
-    wKing: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Chess_klt45.svg/50px-Chess_klt45.svg.png"
-    };
-
-class Piece {
-    constructor(row, col, type, side) {
-        this.row = row;
-        this.col = col;
-        this.type = type;
-        this.side = side;
-        eval("this.source = srcPieces." + type);
-    }
-}
-
-function fPiece(cell, source) {
-    let vPiece = document.createElement("img");
-    vPiece.setAttribute("class", "cPiece");
-    vPiece.setAttribute("src", source);
-    cell.appendChild(vPiece);
-
-}
-
-function fClicked(event) {
-    if (vClicked !== undefined) {
-        vClicked.classList.remove("selected");
-    }
-    vClicked = event.currentTarget;
-    vClicked.classList.add("selected");
-}
-
-function fStart () {
-    let i = 0;
-    for (let x in srcPieces) {
-        if (i < 5) {
-            aPieces.push(new Piece(0, i, x, "w"));
-            if (i < 3) {
-                aPieces.push(new Piece(0, 7 - i, x, "w"));
-            }
-        } if (i == 5) {
-            for (let k = 0; k < 8; k++) {
-                aPieces.push(new Piece(1, k, x, "w"));
-            }
-        } if (i == 6) {
-            for (let k = 0; k < 8; k++) {
-                aPieces.push(new Piece(6, k, x, "b"));
-            }            
-        } if (i >= 7) {
-            aPieces.push(new Piece(7, i - 7, x, "b"));
-            if (i < 10) {
-                aPieces.push(new Piece(7, 14 - i, x, "b"));
+//functions
+function fMoves(y,x) {
+    const result = [];
+    const side = aPieces[aDataNew[y][x]][1];
+    const piece = aPieces[aDataNew[y][x]][2];
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            if (i !== y || j !== x) {
+                if (piece == "rook") {
+                    if (i == y || j == x) {
+                        result.push([i,j]);
+                    }
+                } if (piece == "knight") {
+                    if (i !== y && j !== x && Math.abs(i - y) + Math.abs(j - x) == 3) {
+                        result.push([i,j]);
+                    }
+                } if (piece == "bishop") {
+                    if (Math.abs(i - y) == Math.abs(j - x)) {
+                        result.push([i,j]);
+                    }
+                }  if (piece == "queen") {
+                    if (i == y || j == x || Math.abs(i - y) == Math.abs(j - x)) {
+                        result.push([i,j]);
+                    }
+                } if (piece == "king") {
+                    if (Math.abs(i - y) <= 1 && Math.abs(j - x) <= 1) {
+                        result.push([i,j]);
+                    }
+                } if (piece == "pawn") {
+                    if (side == "white"){
+                        if (y - i == 1 && x == j) {
+                            result.push([i,j]);
+                        }
+                    } else {
+                        if (i - y == 1 && x == j) {
+                            result.push([i,j]);
+                        }
+                    }
+                }
             }
         }
-        i++;
+    }
+    return result;
+}
+
+function fClicked(event, i,j) {
+    if (event.currentTarget.classList[1] == "selected") {
+        vClicked[0].classList.remove("selected");
+        for (let loc of vClicked[1]) {
+            let eTd = document.getElementById(loc[0] + "-" + loc[1]);
+            eTd.classList.remove("cMoves");
+        }
+    } else {
+        if (vClicked[0] !== undefined) {
+            vClicked[0].classList.remove("selected");
+            for (let loc of vClicked[1]) {
+                let eTd = document.getElementById(loc[0] + "-" + loc[1]);
+                eTd.classList.remove("cMoves");
+            }
+        }
+        vClicked[0] = event.currentTarget;
+        vClicked[0].classList.add("selected");
+        vClicked[1] = fMoves(i,j);
+        for (let loc of vClicked[1]) {
+            let eTd = document.getElementById(loc[0] + "-" + loc[1]);
+            eTd.classList.add("cMoves");
+        }
     }
 }
 
-function stage0() {
-    let eTr;
-    let eTd;
-    let vId;
-
-    let eTable = document.createElement("table");
+function fMakeTable(data) {
+    const eTable = document.createElement("table");
     document.body.appendChild(eTable);
-    for (let j = 0; j < 8; j++) {
-        eTr = document.createElement("tr");
+    for (let i = 0; i < 8; i++) {
+        let eTr = document.createElement("tr");
         eTable.appendChild(eTr);
-        for (let i = 0; i < 8; i++) {
-            eTd = document.createElement("td");
-            vId++;
-            eTd.id = vId.toString(); 
-            if ( (i+j)%2 == 0) {
-                eTd.className = "white";
-            } else {
-                eTd.className = "black";
-            }
+        for (let j = 0; j < 8; j++) {
+            let eTd = document.createElement("td");
             eTr.appendChild(eTd);
-            eTd.addEventListener("click",fClicked);
+            eTd.className = (i+j)%2 == 0 ? "white" : "black";
+            eTd.id = i + "-" + j;
+            if (data[i][j] !== undefined) {
+                let eImg = document.createElement("img");
+                eImg.src = aPieces[data[i][j]][3];
+                eImg.className = "cPiece";
+                eTd.appendChild(eImg);
+                eTd.addEventListener("click", (event) => fClicked(event,i,j));
+            }
         }
     }
-    fStart();
-    for (let x of aPieces) {
-        fPiece(eTable.rows[x.row].cells[x.col],x.source);
-    }
+} 
+
+function fOnLoad() {
+    fMakeTable(aDataStart);
 
 }
 
-window.addEventListener("load", stage0);
+//arrays
+const aDataStart = [
+    [0,1,2,3,4,2,1,0],
+    [5,5,5,5,5,5,5,5],
+    [undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined],
+    [undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined],
+    [undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined],
+    [undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined],
+    [6,6,6,6,6,6,6,6],
+    [7,8,9,10,11,9,8,7]
+];
+const aDataNew = aDataStart;
+
+const aPieces = [
+    [0, "black", "rook", "https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Chess_rdt45.svg/50px-Chess_rdt45.svg.png"],
+    [1, "black", "knight", "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Chess_ndt45.svg/50px-Chess_ndt45.svg.png"],
+    [2, "black", "bishop", "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Chess_bdt45.svg/50px-Chess_bdt45.svg.png"],
+    [3, "black", "queen", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Chess_qdt45.svg/50px-Chess_qdt45.svg.png"],
+    [4, "black", "king", "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Chess_kdt45.svg/50px-Chess_kdt45.svg.png"],
+    [5, "black", "pawn", "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Chess_pdt45.svg/50px-Chess_pdt45.svg.png"],
+    [6, "white", "pawn", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Chess_plt45.svg/50px-Chess_plt45.svg.png"],
+    [7, "white", "rook", "https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Chess_rlt45.svg/50px-Chess_rlt45.svg.png"],
+    [8, "white", "knight", "https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Chess_nlt45.svg/50px-Chess_nlt45.svg.png"],
+    [9, "white", "bishop", "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Chess_blt45.svg/50px-Chess_blt45.svg.png"],
+    [10, "white", "queen", "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Chess_qlt45.svg/50px-Chess_qlt45.svg.png"],
+    [11, "white", "king", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Chess_klt45.svg/50px-Chess_klt45.svg.png"]
+];
+
+//variables
+let vClicked = [undefined,undefined];
+
+//load
+window.addEventListener("load", fOnLoad);
