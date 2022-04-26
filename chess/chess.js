93f06b2046183990f55bd4c2555fabaaf80b0cfb
avrahamleibman <102976517+avrahamleibman@users.noreply.github.com>
2022-04-26 16:04:36 +0300

    turns + under check limitations

--- chess/chess.js
+++ chess/chess.js
@@ -4,6 +4,9 @@
 //eApple - element | fApple - function | aApple - array
 //oApple - object | cApple - class(css)
 
+// aDataNew[a][b] is an index of a piece
+// aPieces[ aDataNew[a][b] ][c] is some property of that piece
+
 function fMakeBoard(data) {
     const eTable = document.createElement("table");
     document.body.appendChild(eTable);
@@ -27,7 +30,7 @@ function fCellProperties(i,j,eTd,data) {
 }
 function fAddIcon(i,j,eTd,data) {
     let eImg = document.createElement("img");
-    eImg.src = aPieces[data[i][j]][3]; //adds source from data
+    eImg.src = aPieces[data[i][j]][3]; //adds source from aPieces
     eImg.className = "cPiece";
     eTd.appendChild(eImg);
 }
@@ -45,7 +48,7 @@ function fOnClick(event, i,j) {
         }
         fClearSelected();
     }
-    if (aDataNew[i][j] != undefined) {
+    if (aDataNew[i][j] != undefined && aPieces[aDataNew[i][j]][1] == vTurn ) {
         fSelectCell(event, i,j);
     }
 }
@@ -59,12 +62,18 @@ function fClearSelected() {
         eTd.classList.remove("cMove");
     }
     oSelected.moves = [];
+    for (let loc of oSelected.underCheck) {
+        let eTd = document.getElementById(loc[0] + "-" + loc[1]);
+        eTd.classList.remove("cUnderCheck");
+    }
+    oSelected.underCheck = [];
 }
 function fMakeAMove(i, j) {
     //moves piece in data
     aDataNew[i][j] = aDataNew[oSelected.y][oSelected.x];
     aDataNew[oSelected.y][oSelected.x] = undefined;
     fClearSelected();
+    vTurn = vTurn == "white" ? "black" : "white";
     //destroys and creates a board from data
     let eTable = document.getElementsByTagName("table");
     eTable[0].remove();
@@ -75,17 +84,26 @@ function fSelectCell(event, i,j) {
     oSelected.y = i;
     oSelected.x = j;
     oSelected.cell.classList.add("cSelected");
-    oSelected.moves = fGenerateMoves(i,j);
+    for (let move of fPieceMoves(aDataNew,i,j)) {
+        if (fIsMoveLegal(move)) {
+            oSelected.moves.push(move);
+        } else {
+            oSelected.underCheck.push(move);
+        }
+    }
     for (let loc of oSelected.moves) {
         let eTd = document.getElementById(loc[0] + "-" + loc[1]);
         eTd.classList.add("cMove");
     }
+    for (let loc of oSelected.underCheck) {
+        let eTd = document.getElementById(loc[0] + "-" + loc[1]);
+        eTd.classList.add("cUnderCheck");
+    }
 }
 
-
-function fGenerateMoves(y,x) {
+function fPieceMoves(data,y,x) {
     let result = [];
-    const index = aDataNew[y][x]
+    const index = data[y][x];
     const side = aPieces[index][1];
     const piece = aPieces[index][2];
     const aDir = aPieces[index][4];
@@ -95,10 +113,13 @@ function fGenerateMoves(y,x) {
         i = dir[0];  //set to a direction
         j = dir[1];
         while (y+i >= 0 && y+i < 8 && x+j >= 0 && x+j < 8) { //goes while location inside board
-            if (aDataNew[y+i][x+j] != undefined) { //stops if finds a piece
-                if (aPieces[aDataNew[y+i][x+j]][1] != side) { //can capture if enemy piece
+            if (data[y+i][x+j] != undefined) { //stops if finds a piece
+                if (aPieces[data[y+i][x+j]][1] != side) { //can capture if enemy piece
                     if (piece != "pawn" || j != 0) { //pawn can't capture ahead
                         result.push([y+i,x+j]); //capure
+                        if (data[y+i][x+j] == 4 || data[y+i][x+j] == 11) { //capured piece is enemy king
+                            aCheck[0] = 1; //king is under check
+                        }
                     }
                 }
                 break; //pieces can't move through other pieces
@@ -116,6 +137,33 @@ function fGenerateMoves(y,x) {
     }
     return result;
 }
+function fIsMoveLegal(move) {
+    //imagines a move
+    const aDataTemp = [];
+    for (let index of aDataNew) {
+        aDataTemp.push([...index]);
+    }
+    aDataTemp[move[0]][move[1]] = aDataTemp[oSelected.y][oSelected.x];
+    aDataTemp[oSelected.y][oSelected.x] = undefined;
+    //is king in check after the move?
+    let opponent = vTurn == "white" ? "black" : "white";
+    return fGivesCheck(aDataTemp, opponent);
+}
+
+function fGivesCheck(aDataTemp, side) { 
+    aCheck[0] = 0;
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            if (aDataTemp[i][j] != undefined) {
+                if (aPieces[aDataTemp[i][j]][1] == side) {
+                    fPieceMoves(aDataTemp,i,j);
+                }
+            }
+        } 
+    }
+    return aCheck[0] == 0;
+}
+
 
 //pieces location on board by index
 const aDataStart = [
@@ -128,7 +176,7 @@ const aDataStart = [
     [6,6,6,6,6,6,6,6],
     [7,8,9,10,11,9,8,7]
 ];
-let aDataNew = [];
+const aDataNew = [];
 for (let index of aDataStart) {
     aDataNew.push([...index]);
 }
@@ -155,8 +203,13 @@ const oSelected = {
     cell: undefined,
     y: undefined,
     x: undefined,
-    moves: []
+    moves: [],
+    underCheck: []
 };
+
+// ver
+let vTurn = "white";
+const aCheck = [0];
 
 //load
 window.addEventListener("load", () => {
