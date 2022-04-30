54cd0e7d2f5b9ea29b131b8348ba7a08fa73da86
avrahamleibman <102976517+avrahamleibman@users.noreply.github.com>
2022-04-30 17:50:00 +0300

    chackmate fix + check and stalemate announcement

--- chess/chess.js
+++ chess/chess.js
@@ -18,12 +18,6 @@ function fMakeBoard(data) {
         }
     }
 }
-function fRemakeBoard(data) {
-    fIsLost(vTurn);
-    let eTable = document.getElementsByTagName("table");
-    eTable[0].remove();
-    fMakeBoard(data);
-}
 function fCellProperties(i,j,eTd,data) {
     eTd.className = (i+j)%2 == 0 ? "white" : "black"; // sets alternating classes
     eTd.id = i + "-" + j;
@@ -174,12 +168,12 @@ function fIsMoveLegal(move) {
     let opponent = vTurn == "white" ? "black" : "white";
     return ! fGivesCheck(aDataTemp, opponent);
 }
-function fGivesCheck(aDataTemp, side) { 
+function fGivesCheck(data, side) { 
     aCheck[0] = false;
     for (let i = 0; i < 8; i++) {
         for (let j = 0; j < 8; j++) {
-            if (aDataTemp[i][j] != undefined && aPieces[aDataTemp[i][j]][1] == side) {
-                fPieceMoves(aDataTemp,i,j);
+            if (data[i][j] != undefined && aPieces[data[i][j]][1] == side) {
+                fPieceMoves(data,i,j);
             }
         } 
     }
@@ -210,11 +204,33 @@ function fPromote(i,j,x) {
     }
 }
 
-function fIsLost(side) {
+function fRemakeBoard(data) {
+    //remove check announcement
+    let eCheck = document.getElementsByClassName("check");
+    if (eCheck[0] != undefined) eCheck[0].remove();
+    //make announcement check/mate/Stalemate
+    let opponent = vTurn == "white" ? "black" : "white";
+    let vCanMove = fCanMove(vTurn);
+    let vUnderCheck = fGivesCheck(aDataNew, opponent)
+    if ( ! vCanMove ) {
+        if (vUnderCheck) {
+            fChackmate();
+        } else {
+            fStalemate();
+        }
+    } else if (vUnderCheck) {
+        fCheck();
+    }
+    //remakes the board
+    let eTable = document.getElementsByTagName("table");
+    eTable[0].remove();
+    fMakeBoard(data);
+}
+function fCanMove(side) {
     sideMoves = [];
     for (let i = 0; i < 8; i++) {
         for (let j = 0; j < 8; j++) {
-            if (aDataNew[i][j] != undefined && aPieces[aDataNew[i][j]][1] == vTurn) {
+            if (aDataNew[i][j] != undefined && aPieces[aDataNew[i][j]][1] == side) {
                 oSelected.y = i;
                 oSelected.x = j;
                 for (let move of fPieceMoves(aDataNew,i,j)) {
@@ -227,17 +243,28 @@ function fIsLost(side) {
     }
     oSelected.y = undefined;
     oSelected.x = undefined;
-    let opponent = side == "white" ? "black" : "white";
     if (sideMoves.length == 0) {
-        fWin(opponent);
+        return false;
     }
+    return true;
 }
-function fWin(side) {
+function fCheck() {
+    let eCheck = document.createElement("div");
+    document.body.appendChild(eCheck);
+    eCheck.className ="check";
+    eCheck.innerText = "chack!";
+}
+function fStalemate() {
+    let eStaleMate = document.createElement("div");
+    document.body.appendChild(eStaleMate);
+    eStaleMate.className ="stalemate";
+    eStaleMate.innerText = "stalemate!";
+}
+function fChackmate() {
     let eWon = document.createElement("div");
     document.body.appendChild(eWon);
     eWon.className ="won";
-    eWon.innerText = "chackmate! " + side + " has won"
-    console.log(side + " has won")
+    eWon.innerText = "chackmate!";
 }
 
 //pieces location on board by index
